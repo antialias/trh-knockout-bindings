3402f88c7bcf67cf271386aee857e65c32cb54f2
antialias <thomas.hallock@kaplan.com>
2013-07-25 17:02:52 -0400

    POD-2227 validation binding now takes lists of properties

--- ko.validation.js
+++ ko.validation.js
@@ -1,23 +1,30 @@
-define(['knockout'], function (ko) {
+define([
+	'knockout',
+	'/js/lp/lib/utils.js'
+], function (ko, lpUtils) {
 	ko.bindingHandlers.validation = {
 	    init: function(element, valueAccessor) {
-			var $tooltipError = $("<div>").addClass('tooltip-error');
-			$(element).data("$tooltip-error", $tooltipError);
-			$tooltipError.append($("<span>").html(valueAccessor().message));
-			$tooltipError.append("<div>")
-			$(element).after($tooltipError);
-			if ("static" === $(element).parent().css('position')) {
-				$(element).parent().css('position', 'relative'); // TRH: we are playing with fire here...
-			}
+			$.each(lpUtils.asArray(valueAccessor()), function (i, properties) {
+				var $tooltipError = $("<div>").addClass('tooltip-error');
+				$(element).data("$tooltip-error-" + properties.key, $tooltipError);
+				$tooltipError.append($("<span>").html(properties.message));
+				$tooltipError.append("<div>")
+				$(element).after($tooltipError);
+				if ("static" === $(element).parent().css('position')) {
+					$(element).parent().css('position', 'relative'); // TRH: we are playing with fire here...
+				}
+			});
 	    },
 	    update: function(element, valueAccessor, allBindingsAccessor) {
-			valueAccessor().errors.subscribe(function (newErrorsList) {
-				if (-1 !== $.inArray(valueAccessor().key, newErrorsList)) {
-					$(element).data("$tooltip-error").show()
-				} else {
-					$(element).data("$tooltip-error").hide()
-				}
-			})
+			$.each(lpUtils.asArray(valueAccessor()), function (i, properties) {
+				properties.errors.subscribe(function (newErrorsList) {
+					if (-1 !== $.inArray(properties.key, newErrorsList)) {
+						$(element).data("$tooltip-error-" + properties.key).show()
+					} else {
+						$(element).data("$tooltip-error-" + properties.key).hide()
+					}
+				});
+			});
 	    }
 	};
 });
