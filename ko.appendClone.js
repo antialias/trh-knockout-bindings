1c0c5dd42b230e69aa56e2945d285551c357ca37
Thomas Hallock <github@thomashallock.com>
2013-10-22 17:55:56 -0400

    POD-2560 removed xml stringification in when clonging becuase the item shouldn't be xml at this point.

--- ko.appendClone.js
+++ ko.appendClone.js
@@ -1,7 +1,7 @@
 define(['knockout', '/js/lp/lib/utils.js'], function (ko, lpUtils) {
 	ko.bindingHandlers.appendClone = {
 		init: function(element, valueAccessor) {
-			jQuery(element).append(lpUtils.xmlStringifier(jQuery(ko.utils.unwrapObservable(valueAccessor)()).get(0)));
+			jQuery(element).append(jQuery(ko.utils.unwrapObservable(valueAccessor())).clone());
 		},
 		update: function(element, valueAccessor, allBindingsAccessor) { }
 	};
